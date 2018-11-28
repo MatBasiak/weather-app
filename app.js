10bea59e0f59b0d4d2962e0271407c6946dc61e5
mateusz <mateusz.basiak@gmail.com>
2018-11-28 21:26:39 +0100

    remove api key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const request = require('request'); // npm package
-const key = 'AIzaSyCCtmmattvLoQZpN2ty22hUb29ssDE8f6A'; // my api key from google cloud 
+const key = ''; // my api key from google cloud 
 
 
 const yargs = require('yargs');
@@ -25,6 +25,7 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=${address}`,
     json: true
 }, (error, response, body) => {
+    
     // console.log(JSON.stringify(body,undefined,2));
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`The location of ${body.results[0].address_components[0].long_name} is `);
