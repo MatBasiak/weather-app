2cfba5805c93d99b4789b4f80efa7884d0f58e2e
mateusz <mateusz.basiak@gmail.com>
2018-11-28 22:24:25 +0100

    split app

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const request = require('request');
-const config= require('./config.js')
-const key = `${config.MY_KEY}`; // my api key from google cloud 
-
-
+const geocode = require('./geocode/geocode')
 const yargs = require('yargs');
 
 const argv = yargs
@@ -19,24 +15,4 @@ const argv = yargs
     .alias('help', "h")
     .argv;
 
-
-    let address=encodeURIComponent(argv.address);// add % in place of " " oposite function is decodeUriComponent which put " " in place of %
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=${address}`,
-    json: true
-}, (error, response, body) => {
-    if(error){
-        console.log('unable to connect to google servers');
-        
-    }else if(body.status === 'ZERO_RESULTS'){
-        console.log('unable to find that address');
-    }else if(body.status === 'OK'){
-        
-    // console.log(JSON.stringify(body,undefined,2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`The location of ${body.results[0].address_components[0].long_name} is `);
-    console.log(`Lattitude is ${body.results[0].geometry.location.lat} `);
-    console.log(`Longitude is ${body.results[0].geometry.location.lng}`);
-    }
-});
+geocode.geocodeAddress(argv.address);
---
+++ geocode/geocode.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const config = require('../config')
+const key = `${config.MY_KEY}`; // my api key from google cloud 
+
+
+const geocodeAddress = (address) => {
+
+    encodedAddress = encodeURIComponent(address); // add % in place of " " oposite function is decodeUriComponent which put " " in place of %
+
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=${encodedAddress}`,
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            console.log('unable to connect to google servers');
+
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log('unable to find that address');
+        } else if (body.status === 'OK') {
+
+            // console.log(JSON.stringify(body,undefined,2));
+            console.log(`Address: ${body.results[0].formatted_address}`);
+            console.log(`The location of ${body.results[0].address_components[0].long_name} is `);
+            console.log(`Lattitude is ${body.results[0].geometry.location.lat} `);
+            console.log(`Longitude is ${body.results[0].geometry.location.lng}`);
+        }
+    });
+};
+
+module.exports = {
+    geocodeAddress
+}
