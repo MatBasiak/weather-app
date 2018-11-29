516dc06c24d38480320c5961d68bb9a613549641
mateusz <mateusz.basiak@gmail.com>
2018-11-29 18:47:33 +0100

    conect goe api whith weather api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const geocode = require('./geocode/geocode')
 const yargs = require('yargs');
-const request = require('request');
-const config = require('./config')
-const key = `${config.DARK_KEY}`;
+
+const weather = require('./weather/weahter')
+
 
 const argv = yargs
     .options({
@@ -22,26 +22,20 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2))
-        
+        // console.log(JSON.stringify(results, undefined, 2))
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(JSON.stringify(weatherResults, undefined, 2))
+                console.log(`it's currently ${(weatherResults.temperature-32)/1.8} celsjus. It feel like ${(weatherResults.apparentTemperature-32)/1.8} celsjus`);
+
+            }
+
+
+        });
+
     }
 
 });
-
-request({
-            url: `https://api.darksky.net/forecast/${key}/51.8267,22.233`,
-            json: true
-        }, (error, response, body) => {
-            if (error) {
-               console.log('unable to connect darksky servers');
-
-            } else if (body.error === 'The given location is invalid.') {
-                console.log('unable to find that address');
-            } else if (body != "") {
-               
-                   console.log( 'temperature:', body.currently.temperature);
-                   console.log( 'latitude:', body.longitude);
-                 console.log('longitude:', body.currently.summary);
-            }})
-                
-            
---
+++ weather/weahter.js
@@ -0,0 +1,40 @@
+const request = require('request');
+const config = require('../config')
+const key = `${config.DARK_KEY}`;
+
+
+const getWeather = (lat, lng, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/${key}/${lat},${lng}`,
+        json: true
+    }, (error, response, body) => {
+        // if (error) {
+        //     console.log('unable to connect darksky servers');
+
+        // } else if (body.error === 'The given location is invalid.'//response.statusCode===400) {
+        //     console.log('unable to fetch weather');
+        // } else if (response.statusCode === 200) {
+
+        //     console.log('temperature:', body.currently.temperature);
+        //     console.log('latitude:', body.longitude);
+        //     console.log('summary:', body.currently.summary);
+        // }
+
+        if (!error && response.statusCode === 200) {
+            callback(undefined,{
+
+                temperature: body.currently.temperature,
+                latitude: body.longitude,
+                summary: body.currently.summary,
+                apparentTemperature: body.currently.apparentTemperature
+
+            })
+
+
+        } else {
+            callback('Enable to fetch the weather.');
+        }
+    });
+};
+
+module.exports.getWeather = getWeather;
